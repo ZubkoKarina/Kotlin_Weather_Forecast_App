7e3d799130831ef9566219a662d57a41a15521da
Karina Zubko <karinazubko2017@gmail.com>
2023-09-11 23:00:10 +0300

    New files and utilities are described

--- app/src/main/java/com/example/weather_forecast_app/Utils.kt
+++ app/src/main/java/com/example/weather_forecast_app/Utils.kt
@@ -1,3 +1,8 @@
 package com.example.weather_forecast_app
 class Utils {
+    companion object{
+        var BASE_URL : String = "https://api.openweathermap.org/data/2.5/"
+        var API_KEY  : String = ""
+        const val PERMISSION_REQUEST_CODE = 123
+    }
 }

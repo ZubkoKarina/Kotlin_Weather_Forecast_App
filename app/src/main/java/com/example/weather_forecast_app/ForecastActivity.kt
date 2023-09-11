cb5bffbac4c10105ff7775718dac051faab26e07
Karina Zubko <karinazubko2017@gmail.com>
2023-09-11 16:44:45 +0300

    New configurations and files added

--- app/src/main/java/com/example/weather_forecast_app/ForecastActivity.kt
+++ app/src/main/java/com/example/weather_forecast_app/ForecastActivity.kt
@@ -1,11 +1,11 @@
 package com.example.weather_forecast_app
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 
-class MainActivity : ComponentActivity() {
+class ForecastActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_forecast)
     }
 }

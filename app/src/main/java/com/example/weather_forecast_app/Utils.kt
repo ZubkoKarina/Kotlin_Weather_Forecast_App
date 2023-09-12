3f75f5b01fc214f6f1a05a8f6a61e8776ad4d6db
Karina Zubko <karinazubko2017@gmail.com>
2023-09-12 14:20:58 +0300

    Exclude Utils.kt from tracking

---
+++ app/src/main/java/com/example/weather_forecast_app/Utils.kt
@@ -0,0 +1,8 @@
+package com.example.weather_forecast_app
+class Utils {
+    companion object{
+        var BASE_URL : String = "https://api.openweathermap.org/data/2.5/"
+        var API_KEY  : String = "1"
+        const val PERMISSION_REQUEST_CODE = 123
+    }
+}

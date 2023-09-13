026941197f5167b6d286aebc0bf3b478b802d6d8
Karina Zubko <karinazubko2017@gmail.com>
2023-09-13 16:59:32 +0300

    Window expansion changes v2

--- app/src/main/java/com/example/weather_forecast_app/MainActivity.kt
+++ app/src/main/java/com/example/weather_forecast_app/MainActivity.kt
@@ -13,6 +13,10 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
@@ -51,6 +55,19 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+
+
+
+
+
+
+
         binding = DataBindingUtil.setContentView(this, R.layout.testlayout)
         viM = ViewModelProvider(this).get(WeatherVm::class.java)
         viM.getWeather()
